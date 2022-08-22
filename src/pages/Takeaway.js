c52600d3fe5f8e55f9f0522e56f9123c80a44257
tdgog <tdgog321@gmail.com>
2022-08-22 11:35:44 +0100

    takeaways page

--- src/pages/Takeaway.js
+++ src/pages/Takeaway.js
@@ -1,9 +1,17 @@
 import { Typography } from "@mui/material"
+import { useNavigate } from 'react-router-dom';
 
 function Card(props){
+    let navigate = useNavigate();
     const image = props.image || '';
     const color = props.color || 'bg-bs';
-    return <div className={`h-44 w-96 ${color} bg-cover rounded-lg overflow-hidden flex justify-center items-center`}>
+    const goto = props.goto || null;
+    return <div
+        className={`h-44 w-96 ${color} bg-cover rounded-lg overflow-hidden flex justify-center items-center cursor-pointer`}
+        onClick={() => {
+            navigate(`/redirect/${encodeURIComponent(goto)}`);
+        }}
+    >
         <img src={image} className='h-52 w-52' />
     </div>
 }
@@ -11,11 +19,29 @@ function Card(props){
 export default function Takeaway(){
     return <div className="h-screen w-screen bg-background bg-cover bg-fixed p-5 pt-20">
         <Typography color='white' variant='h2'>Takeaways</Typography>
-        <Typography color='white' variant='h6'>desc</Typography>
+        <Typography color='white' variant='h6'>Order Bean and Brew directly to your door with our trusted delivery partners</Typography>
 
-        <Card 
-            image='./Deliveroo.png'
-            color='bg-[#00CDBC]'
-        />
+        <div className='h-cardheight w-cardwidth mt-10 grid grid-cols-2 gap-2'>
+            <Card 
+                image='./DeliveryCompanies/Deliveroo.png'
+                color='bg-[#00CDBC]' 
+                goto='https://deliveroo.co.uk/'
+            />
+            <Card 
+                image='./DeliveryCompanies/justeat.png'
+                color='bg-[#EF7204]' 
+                goto='https://www.just-eat.co.uk/'
+            />
+            <Card 
+                image='./DeliveryCompanies/ubereats.png'
+                color='bg-[#142328]' 
+                goto='https://www.ubereats.com/gb'
+            />
+            <Card 
+                image='./DeliveryCompanies/foodhub.png'
+                color='bg-[#D82927]' 
+                goto='https://foodhub.co.uk/'
+            />
+        </div>
     </div>
 }
